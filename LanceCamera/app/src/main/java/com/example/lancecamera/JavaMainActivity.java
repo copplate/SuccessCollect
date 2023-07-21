430817c8ce6490c461f8dc8afccf69f390aa81b1
yuancheng <674678976@qq.com>
2023-07-21 16:04:49 +0800

    享学lance讲的mediacodec

---
+++ LanceCamera/app/src/main/java/com/example/lancecamera/JavaMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.lancecamera;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JavaMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_java_main);
+    }
+}
